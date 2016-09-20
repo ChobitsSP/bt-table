a735239f7c74cd26ebaf41c2a9f49913790abcb7
Chobits <chobits@live.cn>
2016-09-20 15:17:53 +0800

    main.js

---
+++ src/components/btColSort.js
@@ -0,0 +1,39 @@
+﻿export default function () {
+    return {
+        scope: {
+            caption: '@?',
+            fieldName: '@?',
+            sortable: '=?',
+        },
+        restrict: 'A',
+        templateUrl: require('./btColSort.html'),
+        link: function (scope, element, attr, ctrl) {
+
+            scope.is_sortable = !!scope.sortable;
+            scope.sort_class = '';
+
+            if (scope.is_sortable) {
+                element.bind('click', function () {
+                    if (scope.sortable.sort_name === scope.fieldName) {
+                        scope.sortable.is_desc = !scope.sortable.is_desc;
+                        scope.sort_class = scope.sortable.is_desc ? 'desc' : 'asc';
+                    }
+                    else {
+                        scope.sortable.is_desc = false;
+                        scope.sortable.sort_name = scope.fieldName;
+                    }
+                    scope.$apply();
+                })
+
+                scope.$watch('sortable.sort_name', function (newVal) {
+                    if (scope.fieldName !== newVal) {
+                        scope.sort_class = '';
+                    }
+                    else {
+                        scope.sort_class = scope.sortable.is_desc ? 'desc' : 'asc';
+                    }
+                })
+            }
+        }
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 var path = require('path')
 
 module.exports = {
-    entry: './app.js',
+    entry: './src/main.js',
     resolve: {
         extensions: ['', '.js'],
         alias: {
