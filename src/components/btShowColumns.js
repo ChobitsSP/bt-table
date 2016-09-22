ef7768f9a054beb53848d77e9a7f0d21938ca163
Chobits <chobits@live.cn>
2016-09-22 11:03:01 +0800

    custom dropdown

--- src/components/btShowColumns.js
+++ src/components/btShowColumns.js
@@ -1,12 +1,23 @@
-﻿module.exports = function () {
+﻿import $ from 'utils/NodeList.js'
+
+module.exports = function () {
     return {
         restrict: 'E',
         replace: true,
         scope: {
+            show: '=?',
             columns: '='
         },
         templateUrl: require('./btShowColumns.html'),
-        link: function (scope) {
+        link: function (scope, element) {
+            scope.show = false
+
+            const $el = $(element[0])
+            $el.onBlur((e) => {
+                scope.show = false
+                scope.$apply()
+            })
+
             // if (angular.isArray(scope.columns)) {
             //     angular.forEach(scope.columns, function (col) {
             //         if (!col.hasOwnProperty('visible')) {
