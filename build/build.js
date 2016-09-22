16c3b8d123d530de03a2a1cdc3a94d948c309cd5
Chobits <chobits@live.cn>
2016-09-22 14:06:17 +0800

    add build files

---
+++ build/build.js
@@ -0,0 +1,20 @@
+﻿var path = require('path')
+var config = require('../webpack.config.js')
+var webpack = require('webpack')
+
+console.log(
+  '  Tip:\n' +
+  '  Built files are meant to be served over an HTTP server.\n' +
+  '  Opening index.html over file:// won\'t work.\n'
+)
+
+webpack(config, function (err, stats) {
+  if (err) throw err
+  process.stdout.write(stats.toString({
+    colors: true,
+    modules: false,
+    children: false,
+    chunks: false,
+    chunkModules: false
+  }) + '\n')
+})
