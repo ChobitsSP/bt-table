9ff9b0128af30c380ef4fe6df228e50696fd5e60
Chobits <chobits@live.cn>
2016-09-22 10:57:07 +0800

    isopen -> show

--- src/components/btDropdown.js
+++ src/components/btDropdown.js
@@ -1,17 +1,24 @@
+import $ from 'utils/NodeList.js'
+
 module.exports = function () {
     return {
         restrict: 'E',
         scope: {
             btnTxt: '=btnTxt',
-            isOpen: '=?',
+            show: '=?',
         },
         transclude: true,
         templateUrl: require('./btDropdown.html'),
         link: function (scope, element, attr, ctrl) {
+            const $el = $(element[0])
+            $el.onBlur((e) => { 
+                scope.show = false 
+                scope.$apply()
+            })
             //// WAI-ARIA
             //element.attr({ 'aria-haspopup': true, 'aria-expanded': false });
-            //scope.$watch('isOpen', function (isOpen) {
-            //    element.attr('aria-expanded', !!isOpen);
+            //scope.$watch('show', function (show) {
+            //    element.attr('aria-expanded', !!show);
             //});
         }
     }
