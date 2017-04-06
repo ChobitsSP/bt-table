9f9321219f1665594f69f9484b8865084f600274
Chobits <chobits@live.cn>
2017-04-06 09:50:22 +0800

    debug

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,20 @@ export default {
     name: 'app'
 }
 </script>
+
+<style>
+    [ng\:cloak],
+    [ng-cloak],
+    [data-ng-cloak],
+    [x-ng-cloak],
+    .ng-cloak,
+    .x-ng-cloak,
+    .ng-hide {
+        display: none !important;
+    }
+
+    .bs-radio {
+        text-align: center;
+        width: 10px;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
-﻿
+﻿import btTable from '@components/btTable';
+import btCol from '@components/btCol';
+import btPager from '@components/btPager';
+import btDropdown from '@components/btDropdown';
+import btShowColumns from '@components/btShowColumns';
+import btColSort from '@components/btColSort';
 
+import btRow from '@directives/btRow';
+import checkboxAll from '@directives/checkboxAll';
 
-console.log(123);
+if(window.angular) {
+    let app = angular.module('bt-table', [])
+
+    app.directive('btTable', btTable)
+    app.directive('btCol', btCol)
+    app.directive('btPager', btPager)
+    app.directive('btDropdown', btDropdown)
+    app.directive('btShowColumns', btShowColumns)
+    app.directive('btColSort', btColSort)
+
+    app.directive('btRow', btRow)
+    app.directive('checkboxAll', checkboxAll)
+}
+else {
+    window.BtTable = {
+        
+    }
+}
