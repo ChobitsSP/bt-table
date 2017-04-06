1bf6e9b8ddec40033472439cd4c6d6d001a6c921
Chobits <chobits@live.cn>
2017-04-06 10:09:15 +0800

    debug

--- src/main.prod.js
+++ src/main.prod.js
@@ -1,51 +1,28 @@
+import btTable from '@components/btTable';
+import btCol from '@components/btCol';
+import btPager from '@components/btPager';
+import btDropdown from '@components/btDropdown';
+import btShowColumns from '@components/btShowColumns';
+import btColSort from '@components/btColSort';
 
+import btRow from '@directives/btRow';
+import checkboxAll from '@directives/checkboxAll';
 
+if(window.angular) {
+    let app = angular.module('bt-table', [])
 
-import template from '@components/btTable.html';
+    app.directive('btTable', btTable)
+    app.directive('btCol', btCol)
+    app.directive('btPager', btPager)
+    app.directive('btDropdown', btDropdown)
+    app.directive('btShowColumns', btShowColumns)
+    app.directive('btColSort', btColSort)
 
-
-debugger;
-
-
-console.log(template);
-
-
-window.template = template;
-
-
-//import btTable from '@components/btTable';
-//import btCol from '@components/btCol';
-//import btPager from '@components/btPager';
-//import btDropdown from '@components/btDropdown';
-//import btShowColumns from '@components/btShowColumns';
-//import btColSort from '@components/btColSort';
-
-//import btRow from '@directives/btRow';
-//import checkboxAll from '@directives/checkboxAll';
-
-//import Vue from 'vue'
-//import App from './App'
-
-//if(window.angular) {
-//    let app = angular.module('bt-table', [])
-
-//    app.directive('btTable', btTable)
-//    app.directive('btCol', btCol)
-//    app.directive('btPager', btPager)
-//    app.directive('btDropdown', btDropdown)
-//    app.directive('btShowColumns', btShowColumns)
-//    app.directive('btColSort', btColSort)
-
-//    app.directive('btRow', btRow)
-//    app.directive('checkboxAll', checkboxAll)
-//}
-//else {
-//    window.BtTable = {
+    app.directive('btRow', btRow)
+    app.directive('checkboxAll', checkboxAll)
+}
+else {
+    window.BtTable = {
         
-//    }
-//}
-
-//new Vue({
-//    el: '#app',
-//    render: h => h(App)
-//})
+    }
+}
